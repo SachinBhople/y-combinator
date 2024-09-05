0da3faf05f3a63e8d07d7a19be91ddc9ab8b4ec7
sachin <sachinb0841@gmail.com>
2024-09-05 16:15:17 +0530

    routes error fix

--- routes/founder.routes.js
+++ routes/founder.routes.js
@@ -1,11 +1,12 @@
 const founderController = require("../controller/founder.controller")
+const { AdminProtected } = require("../middleware/Protected")
 
 
 const router = require("express").Router()
 
 router
 
-    .post("/create-founder-profile", founderController.createFounderProfile)
+    .post("/create-founder-profile", AdminProtected, founderController.createFounderProfile)
     .get("/get-founder-profile/:id", founderController.getfounderProfile)
     .put("/update-founder-profile/:id", founderController.updatefounderProfile)
     .delete("/delete-founder-profile", founderController.deletefounderProfile)
